5bc0276c22be15d7feabc9c7e6bff92a75462f50
Baltasar Salamon <baltasar.salamonwelwert@gmail.com>
2019-10-09 17:22:40 -0300

    Changed the connector so it gets the first worksheet by number not id

--- Connector/sheetsToSql.py
+++ Connector/sheetsToSql.py
@@ -13,12 +13,12 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name("Quickstart-289b6
 gc = gspread.authorize(credentials)
 
 spr = gc.open_by_url("https://docs.google.com/spreadsheets/d/1Mfy2LE_C0RRB8WIfvdKm72-aOGSFdV27JE9NAmwePnk/edit#gid=227993879")
-wks = spr.worksheet("Global networks - COPY")
+#wks = spr.worksheet("Global networks - COPY").sheet1
 
 engine = create_engine("mysql+pymysql://Baltasar:Pumajaws@Baltasar.mysql.pythonanywhere-services.com/Baltasar$default")
 connection = engine.connect()
 
-list_of_lists = wks.get_all_values()
+list_of_lists = spr.sheet1.get_all_values()
 
 
 
