b9962614d4177e019f876e2ea6c7d4bf09bf5947
Baltasar Salamon <baltasar.salamonwelwert@gmail.com>
2019-10-09 16:15:07 -0300

    sql connection

--- Connector/sheetsToSql.py
+++ Connector/sheetsToSql.py
@@ -14,7 +14,7 @@ gc = gspread.authorize(credentials)
 
 wks = gc.open("Global networks - COPY")
 
-engine = create_engine("mysql+pymysql://{Baltasar}:{Pumajaws}@{Baltasar.mysql.pythonanywhere-services.com}/{Baltasar$PythonDatabase}")
+engine = create_engine("mysql+pymysql://{Baltasar}:{Pumajaws}@{Baltasar.mysql.pythonanywhere-services.com}/{Baltasar$default}")
 
 connection = engine.connect()
 
