301c661e235ef55c58b9cbcc147c867a2cbf2bec
Baltasar Salamon <baltasar.salamonwelwert@gmail.com>
2019-10-10 17:52:50 -0300

    Added export script

---
+++ Connector/ExportSql.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+def get_sql(sql_doc):
+
+    engine = create_engine("mysql+pymysql://Baltasar:Pumajaws@Baltasar.mysql.pythonanywhere-services.com/Baltasar$default")
+    cnxn = engine.connect()
+
+    df = pd.read_sql("{}".format(input("sql command, please don't drop all my tables: ")), cnxn)
+    csvFile = df.to_csv()
+    with open("newFile.csv","w+") as new_file:
+        new_file = csvFile
+    cnxn.close()
