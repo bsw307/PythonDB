b038d2b59c6e0814aa7559a186786b579415a920
Baltasar Salamon <baltasar.salamonwelwert@gmail.com>
2019-10-11 00:24:36 -0300

    Added constructor json module to export

--- Connector/ExportSql.py
+++ Connector/ExportSql.py
@@ -2,11 +2,15 @@ import pandas as pd
 from sqlalchemy import create_engine
 import json
 
+import GenerateJson
 
 def get_sql():
-    
+
+    GenerateJson.create()
+
     with open("Settings.json","r") as json_file:
         var = json.load(json_file)
+        
     hostname = "mysql+pymysql://{}:{}@{}/{}".format(var["database"]["user"],var["database"]["password"],var["database"]["hostname"],var["database"]["database_name"])
     if input("Current hostname is {}. Do you want to change the hostname?y/n".format(hostname) + "\n" ).lower() == "y":
             var = input("New: " + "\n")
