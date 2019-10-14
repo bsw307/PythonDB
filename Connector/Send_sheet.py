2db3b76591a0ef6b76155fbc1936c492c0178e1b
Baltasar Salamon <baltasar.salamonwelwert@gmail.com>
2019-10-14 11:34:15 -0300

    Fixed send

--- Connector/Send_sheet.py
+++ Connector/Send_sheet.py
@@ -52,11 +52,11 @@ def send(Csv_file):
     reader = csv.reader(content, delimiter=',')
     cell_list = worksheet.range('A1:A2')
     tmpcell = []
-    for row in reader:
+    for i, row in reader:
       print("Row: ", row)
-      for column in row:
+      for x, column in row:
         print("column: ", column)
-        tmpcell.append(content[row][column])
+        tmpcell.append(content[i][x])
     for i,val in enumerate(tmpcell):
       cell_list[i].value = val
     worksheet.update_cells(cell_list)
